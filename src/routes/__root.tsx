261ccc182e6c5be73fc53332e73749c90239a71c
Michail Yasonik <michail@yasonik.com>
2026-04-25 17:45:34 -0500

    refactor(header): remove Meeple icon; promote title to h1

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,12 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { Meeple } from "../ui/icons/Meeple";
 import styles from "./index.module.css";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <header role="banner" className={styles.header}>
-        <Meeple className={styles.headerMeeple} aria-hidden="true" />
-        <div>
-          <p>Gen Con Buddy</p>
-          <p>your guide to the best four days in gaming</p>
-        </div>
+        <h1>Gen Con Buddy</h1>
+        <p>your guide to the best four days in gaming</p>
       </header>
       <Outlet />
     </>
